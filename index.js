e209107266f586b0d5179d11255a0bf630394b81
Tanito <ulises.d83@gmail.com>
2020-11-21 20:02:23 -0300

    agregada propiedad imagen

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
-const personajes = require('./personajes')
+const personajes = require('./personajes1')
+//const personajes = require('./api/personajes')
 const hechizos = require('./hechizos')
 const houses = require('./houses')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.send('Welcome to Api Potter');
-});
+app.get('/', function (req, res) { res.send('Bienvenido a Api Potter');});
 
-app.get('/characters', function (req, res) {
-    res.send(personajes);
-});
+app.get('/edit', function (req, res) { res.send(editArray());});
 
-app.get('/houses', function (req, res) {
-    res.send(houses);
-});
+app.get('/characters', function (req, res) {res.send(personajes);});
 
-app.get('/spells', function (req, res) {
-    res.send(hechizos);
-});
+app.get('/houses', function (req, res) {res.send(houses);});
+
+app.get('/spells', function (req, res) {res.send(hechizos);});
 
 app.get("/characters/:character", function (req, res) {
-    var character = req.params.character
-    let personaje = personajes.find(pers => pers.name === character)
+    var character = req.params.character.toLowerCase().replace(' ', '')
+    let personaje = personajes.find(p => p.name.toLowerCase().replace(' ', '') === character)
+  //  console.log('character', character, 'personaje', personaje)
    
     if (personaje) { res.send(personaje) }
     res.redirect("/error")
 })
 
-
 app.get("/houses/:house", function (req, res) {
-    var house = req.params.house
-    let casa = houses.find(c => c.name === house)
+    var house = req.params.house.toLowerCase()
+    let casa = houses.find(c => c.name.toLowerCase() === house)
   
     if (casa) { res.send(casa) }
     res.redirect("/error")
 })
 
 app.get("/spells/:spell", function (req, res) {
-    var spell = req.params.spell
-    let hechizo = hechizos.find(h => h.spell === spell)
+    var spell = req.params.spell.toLowerCase().replace(' ','')
+    let hechizo = hechizos.find(h => h.spell.toLowerCase().replace(' ','') === spell)
   
     if (hechizo) { res.send(hechizo) }
+    res.redirect("/error")
+})
+
+app.get("/src/assets/img/:imagen", function (req, res) {
+    var imagen = req.params.imagen.toLowerCase().replace(' ','')
+    let foto = imagen +'.jpg'
+  
+    
+    if (foto) { res.send(`./src/assets/img/${foto}`) }
     res.redirect("/error")
 })
 
