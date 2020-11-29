0f7fd35d49517ef18cd9d3db415e2bd807658fe5
Tanito <ulises.d83@gmail.com>
2020-11-29 18:44:21 -0300

    agregado para buscar por tipo de hechizo y corregido formato array spells

--- routes/spells.js
+++ routes/spells.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 const hechizos = require('../utils/spells')
 
-router.get('/', function (req, res) { res.send(hechizos); });
+ router.get('/', function (req, res) {
+   
+    const { type } = req.query;
+     
+    if (type) {
+        const filter = hechizos.filter((h) => h.type.toLowerCase() === type.toLowerCase())
+          if (filter.length > 0) {
+            return res.json(filter);
+        } else {
+            return res.sendStatus(404);
+        }
+    }
+    res.send(hechizos);
+}); 
+
 router.get("/:spell", function (req, res) {
     var spell = req.params.spell.toLowerCase().replace(' ', '')
     let hechizo = hechizos.find(h => h.spell.toLowerCase().replace(' ', '') === spell)
